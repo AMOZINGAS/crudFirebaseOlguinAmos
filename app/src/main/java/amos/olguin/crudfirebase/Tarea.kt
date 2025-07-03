b7e4cf9bfcc959cc5d0536ec46d207d77b7d64ed
Amós Helí <amosheli2004@gmail.com>
2025-07-03 16:34:32 -0600

    Inicializacion

---
+++ app/src/main/java/amos/olguin/crudfirebase/Tarea.kt
@@ -0,0 +1,21 @@
+package amos.olguin.crudfirebase
+
+data class Tarea(
+
+    var id: String = "",
+    var titulo: String = "",
+    var descripcion: String = ""
+
+){
+
+    fun toMap(): Map<String, String>{
+
+        return mapOf(
+
+            "titulo" to titulo,
+            "descripcion" to descripcion
+
+        )
+
+    }
+}
